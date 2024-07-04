52fc4a728ca42e61e866aef245fddf2e076042ca
Julius Liias <julius.eliel.liias@gmail.com>
2024-07-04 19:18:51 +0300

    Wrote down information about the character controller project.

--- src/components/info/projects/CharControllerProjectInfo.tsx
+++ src/components/info/projects/CharControllerProjectInfo.tsx
@@ -1,11 +1,99 @@
 import React from "react"
+import ProjectTitle from "../ProjectTitle"
+import ProjectInfoDropdownBox from "../ProjectInfoDropdownBox"
+import { StringReactNodePair } from "../../../utils/Types"
+import ProjectSummary from "../ProjectSummary"
+
+const detailsInfoSet: StringReactNodePair = {
+  string: "Details",
+  node: (
+    <p>
+      This is a small character controller demo I made using Unity and C#.
+      <br />
+      The movement system includes different animations for any rigged humanoid
+      set as the player model.
+      <br />
+      Support for changing between first-person and third-person views.
+    </p>
+  ),
+}
+
+const possibleMovementsInfoSet: StringReactNodePair = {
+  string: "Possible Movement",
+  node: (
+    <>
+      <p>
+        The different types of movements and features of the controller include:
+      </p>
+
+      <li>Smoothed acceleration and deceleration when moving</li>
+      <li>Walking, running and sprinting</li>
+      <li>Going under obstacles by crouching</li>
+      <li>Jump heigh that can be controlled by the button press length</li>
+      <li>Smoothed out air control when the player is airborne</li>
+
+      <p>
+        Everything is exposed and customizable through the editor window on the
+        player controller script.
+      </p>
+    </>
+  ),
+}
+
+const cameraInfoSet: StringReactNodePair = {
+  string: "Camera",
+  node: (
+    <>
+      <p>Camera control and features:</p>
+      <li>Toggling between first-person and third-person</li>
+      <li>
+        Looking around while keeping your movement direction when using the free
+        look mode
+      </li>
+      <li>
+        Dynamic camera preventing occlusion behind obstacles when in
+        third-person
+      </li>
+      <p>
+        The camera also includes a small dynamic field of view change based on
+        the character's speed.
+      </p>
+    </>
+  ),
+}
+
+const inputInfoSet: StringReactNodePair = {
+  string: "Gamepad Input",
+  node: (
+    <p>
+      In addition to mouse and keyboard, the project has Unity's Input System
+      set up to allow the player to control the character with a gamepad.
+    </p>
+  ),
+}
 
 function CharControllerProjectInfo() {
   return (
     <div>
-      <h2>
-        <u>First-Person and Third-Person Character Controller Demo</u>
-      </h2>
+      <ProjectTitle>
+        First-Person and Third-Person Character Controller Demo
+      </ProjectTitle>
+
+      <ProjectSummary>
+        Character controller Unity project.
+        <br />
+        The goal was to create a character controller capable of platforming and
+        running through obstacle courses.
+      </ProjectSummary>
+
+      <ProjectInfoDropdownBox
+        tileInfoSets={[
+          detailsInfoSet,
+          possibleMovementsInfoSet,
+          cameraInfoSet,
+          inputInfoSet,
+        ]}
+      />
     </div>
   )
 }
